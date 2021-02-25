087264ecb961c97536e7181b8f3591ccaf1267bc
Aw Gh <awgh@awgh.org>
2021-02-24 22:42:12 -0800

    Added Linux loader and test. Renamed loader_test to windows_test. That's the hat trick.

---
+++ linux_test.go
@@ -0,0 +1,39 @@
+// +build linux
+
+package universal
+
+import (
+	"io/ioutil"
+	"log"
+	"testing"
+
+	"github.com/awgh/cppgo/asmcall/cdecl"
+)
+
+func Test_Linux_1(t *testing.T) {
+	image, err := ioutil.ReadFile("test/64/main.so")
+
+	loader, err := NewLoader()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	library, err := loader.LoadLibrary("main", &image)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	runmeProc, ok := library.FindProc("Runme")
+	if !ok {
+		t.Fatal("FindProc did not find export Runme")
+	}
+
+	log.Printf("%x %x \n", runmeProc, library.BaseAddress)
+
+	val, err := cdecl.Call(runmeProc, 7)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Printf("%+v\n", val)
+}
--- loader_linux.go
+++ loader_linux.go
@@ -1,6 +1,60 @@
 package universal
 
+import (
+	"bytes"
+	"errors"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+
+	"github.com/Binject/debug/elf"
+)
+
 // LoadLibraryImpl - loads a single library to memory, without trying to check or load required imports
 func LoadLibraryImpl(name string, image *[]byte) (*Library, error) {
-	return nil, nil
+	elflib, err := elf.NewFile(bytes.NewReader(*image))
+	if err != nil {
+		return nil, err
+	}
+	if elflib.FileHeader.Type != elf.ET_DYN {
+		return nil, errors.New("Cannot load non-dynamic libraries")
+	}
+	exports, err := elflib.Exports()
+	if err != nil {
+		return nil, err
+	}
+	pageSize := uint64(unix.Getpagesize())
+	var memSize uint64
+
+	loads := make([]*elf.Prog, 0)
+	for _, p := range elflib.Progs {
+		if p.Type != elf.PT_LOAD || p.Filesz == 0 || p.Memsz == 0 {
+			continue
+		}
+		highest := p.Memsz + p.Vaddr
+		if highest > memSize {
+			memSize = highest
+		}
+		loads = append(loads, p)
+	}
+	if memSize%pageSize != 0 { // round up to page size
+		memSize = ((memSize / pageSize) * pageSize) + pageSize
+	}
+	baseBuf, err := unix.Mmap(-1, 0, int(memSize),
+		unix.PROT_READ|unix.PROT_WRITE|unix.PROT_EXEC, unix.MAP_PRIVATE|unix.MAP_ANON)
+	if err != nil {
+		return nil, err
+	}
+	for _, p := range loads {
+		copy(baseBuf[p.Vaddr:], (*image)[p.Off:p.Off+p.Filesz])
+	}
+	ex := make(map[string]uint64)
+	for _, x := range exports {
+		ex[x.Name] = x.VirtualAddress
+	}
+	library := Library{
+		BaseAddress: uintptr(unsafe.Pointer(&baseBuf[0])),
+		Exports:     ex,
+	}
+	return &library, nil
 }
